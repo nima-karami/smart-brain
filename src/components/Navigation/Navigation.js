8e34c638bab66b2e8809ffc0073b06a257930074
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-07-15 14:50:46 -0400

    first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className="f4 black link pa3 underline pointer">Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
