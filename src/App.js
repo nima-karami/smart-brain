62c184fd0ac0de42f2a6e4455176ec27a42eb090
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-07-18 10:13:22 -0400

    adding ts particles

--- src/App.js
+++ src/App.js
@@ -23,13 +23,88 @@ function App() {
     console.log(container);
   };
 
+  const partcileOptions = {
+    zLayers:1,
+    fpsLimit: 60,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 2,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: false,
+        speed: 2,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 50,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: 'circle',
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+    },
+    detectRetina: true,
+  }
+
   return (
     <div className="App">
-      <Particles id="tsparticles" url="http://foo.bar/particles.json" init={particlesInit} loaded={particlesLoaded} />
       <Navigation />
-      <Logo />
+      <Logo className='z-max'/>
       <Rank />
       <ImageLinkForm />
+      <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={partcileOptions}
+    />
+      
       {/* <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,9 +5,9 @@ import brain from './brain-1.png'
 
 const Logo = () => {
     return (
-        <div className="ma2 mt0 center pointer">
-            <Tilt className="Tilt center" options={{ max : 100, reverse: true}} style={{ height: 200, width: 200 }} >
-                <div className="Tilt-inner pa2"> <img alt='brain logo' src={brain}/> </div>
+        <div className="ma2 mt0 center pointer z-999">
+            <Tilt className="Tilt center z-999" options={{ max : 100, reverse: true}} style={{ height: 200, width: 200 }} >
+                <div className="Tilt-inner pa2 z-999"> <img alt='brain logo z-999' src={brain}/> </div>
             </Tilt>
             <h1>Smart Brain</h1>
         </div>
