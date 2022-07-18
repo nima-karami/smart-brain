2acdcdd7c650c72a4844c0fa0d9cfc4fd709ea2f
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-07-18 09:09:17 -0400

    adding ts particles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank';
+import Particles from 'react-tsparticles'
+import { loadFull } from "tsparticles";
+
 
 function App() {
+  
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div className="App">
+      <Particles id="tsparticles" url="http://foo.bar/particles.json" init={particlesInit} loaded={particlesLoaded} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <ImageLinkForm />
       <FaceRecognition /> */}
